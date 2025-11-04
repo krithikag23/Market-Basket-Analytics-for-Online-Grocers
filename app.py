f2ef583fdceb62dc4430b891f101cc53b96bf33b
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2025-11-04 22:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import time
-
 # ------------------------------------------------------------
 # 🎛️ Sidebar Settings
 # ------------------------------------------------------------
